21af945583b24e4a37ff30730d9e934f0c6532ee
LuisMartinPalafoxVega <lloydb171@gmail.com>
2025-12-03 14:59:54 -0700

    Actualizacion 4

--- backend/app/database.py
+++ backend/app/database.py
@@ -18,7 +18,7 @@ MYSQL_PORT = "3306"
 MYSQL_DB = "railway"
 
 
-DATABASE_URL = f"mysql+pymsql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
