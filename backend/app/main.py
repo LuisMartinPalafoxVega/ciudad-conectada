bd3e9da77937ba3e9723a80b0ee3c64289928e92
LuisMartinPalafoxVega <lloydb171@gmail.com>
2025-12-03 15:46:14 -0700

    Actualizacion 10

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
         "http://localhost:4200",
         "http://127.0.0.1:4200",
         "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://127.0.0.1:8000",
         "https://ciudad-conectada-front-production.up.railway.app"
     ],
     allow_credentials=True,
